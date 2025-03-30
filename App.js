550050cb7f7599ac668167bf05b9497b1e9c32c3
Alexandre Oliveira <alexandre.rmo94@gmail.com>
2025-03-30 13:27:15 -0300

    chore(main): created generate wallet funcion

--- App.js
+++ App.js
@@ -1,20 +1,173 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import React, { Fragment, useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Clipboard from "expo-clipboard";
 
 export default function App() {
+  const [mnemonic, setMnemonic] = useState("");
+  const [wallets, setWallets] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const generateWallets = async () => {
+    try {
+      setLoading(true);
+
+      const { data } = await axios.get(
+        "https://test-crypto-wallet-backend-production.up.railway.app/create-wallet"
+      );
+
+      setMnemonic(data.mnemonic);
+      setWallets(data.wallets);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const copyToClipboard = (text) => {
+    Clipboard.setStringAsync(text);
+    Alert.alert(
+      "Copiado",
+      "O conteúdo foi copiado para a área de transferência."
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Gerador de Carteiras com Frase-semente</Text>
+      <Button title="Gerar Carteiras" onPress={generateWallets} />
+
+      {loading && (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text>Carregando...</Text>
+        </View>
+      )}
+
+      {!!wallets && (
+        <>
+          <View style={styles.row}>
+            <Text style={styles.mnemonic}>Frase-semente: {mnemonic}</Text>
+            <TouchableOpacity
+              style={styles.copyButton}
+              onPress={() => copyToClipboard(mnemonic)}
+            >
+              <Text style={styles.copyButtonText}>Copiar</Text>
+            </TouchableOpacity>
+          </View>
+
+          <ScrollView style={styles.walletList}>
+            {Object.values(wallets).map((wallet, index) => (
+              <View key={index} style={styles.walletCard}>
+                <Text style={styles.walletName}>{wallet.name}</Text>
+
+                <View style={styles.row}>
+                  <Text style={styles.walletAddress}>
+                    Endereço: {wallet.address}
+                  </Text>
+                  <TouchableOpacity
+                    style={styles.copyButton}
+                    onPress={() => copyToClipboard(wallet.address)}
+                  >
+                    <Text style={styles.copyButtonText}>Copiar</Text>
+                  </TouchableOpacity>
+                </View>
+
+                <View style={styles.row}>
+                  <Text style={styles.walletPrivateKey}>
+                    Chave Privada: {wallet.privateKey}
+                  </Text>
+                  <TouchableOpacity
+                    style={styles.copyButton}
+                    onPress={() => copyToClipboard(wallet.privateKey)}
+                  >
+                    <Text style={styles.copyButtonText}>Copiar</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+        </>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+
+  copyButton: {
+    padding: 6,
+    backgroundColor: "#007bff",
+    borderRadius: 4,
+  },
+
+  copyButtonText: {
+    color: "#fff",
+    fontSize: 12,
+  },
+
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+  },
+
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  loadingContainer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  mnemonic: {
+    marginTop: 20,
+    fontSize: 14,
+    color: "#555",
+    flex: 1,
+  },
+  walletList: {
+    maginTop: 20,
+    width: "100%",
+  },
+  walletCard: {
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 1 },
+  },
+  walletName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  walletAddress: {
+    fontSize: 14,
+    marginTop: 5,
+    color: "#555",
+    flex: 1,
+  },
+  walletPrivateKey: {
+    fontSize: 14,
+    marginTop: 5,
+    color: "#777",
+    flex: 1,
   },
 });
