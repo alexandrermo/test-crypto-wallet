ed9823bea5be468ef85da0daad5b5d889cc7c381
Alexandre Oliveira <alexandre.rmo94@gmail.com>
2025-03-30 15:44:55 -0300

    featindex): made open wallets

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -9,14 +9,18 @@ import {
   ActivityIndicator,
   Alert,
   TouchableOpacity,
+  Modal,
+  TextInput,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Clipboard from "expo-clipboard";
 
 export default function App() {
-  const [mnemonic, setMnemonic] = useState("");
+  const [mnemonicOpenned, setMnemonicOpenned] = useState("");
+  const [mnemonicToOpen, setMenemonicToOpen] = useState("");
   const [wallets, setWallets] = useState();
   const [loading, setLoading] = useState(false);
+  const [isOpenInsertMnemonic, setIsOpenInsertMnemonic] = useState(false);
 
   const generateWallets = async () => {
     try {
@@ -26,7 +30,7 @@ export default function App() {
         "https://test-crypto-wallet-backend-production.up.railway.app/create-wallet"
       );
 
-      setMnemonic(data.mnemonic);
+      setMnemonicOpenned(data.mnemonic);
       setWallets(data.wallets);
     } finally {
       setLoading(false);
@@ -41,10 +45,64 @@ export default function App() {
     );
   };
 
+  const openWallets = async () => {
+    try {
+      setLoading(true);
+
+      const { data } = await axios.post(
+        "https://test-crypto-wallet-backend-production.up.railway.app/open-wallet",
+        {
+          mnemonic: mnemonicToOpen,
+        }
+      );
+
+      setMnemonicOpenned(mnemonicToOpen);
+      setMenemonicToOpen("");
+      setIsOpenInsertMnemonic(false);
+      setWallets(data.wallets);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const openInsertMnemonic = () => {
+    setIsOpenInsertMnemonic(true);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      <Modal
+        visible={isOpenInsertMnemonic}
+        animationType="slide"
+        transparent={true}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Digite seu mnemônico</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Digite o mnemônico aqui"
+              value={mnemonicToOpen}
+              onChangeText={setMenemonicToOpen}
+              multiline={true}
+            />
+            <View style={styles.modalButtons}>
+              <Button
+                title="Cancelar"
+                onPress={() => setIsOpenInsertMnemonic(false)}
+              />
+              <Button title="Abrir" onPress={openWallets} />
+            </View>
+          </View>
+        </View>
+      </Modal>
+
       <Text style={styles.header}>Gerador de Carteiras com Frase-semente</Text>
       <Button title="Gerar Carteiras" onPress={generateWallets} />
+
+      <Button title="Abrir Carteiras" onPress={openInsertMnemonic} />
 
       {loading && (
         <View style={styles.loadingContainer}>
@@ -56,10 +114,12 @@ export default function App() {
       {!!wallets && (
         <>
           <View style={styles.row}>
-            <Text style={styles.mnemonic}>Frase-semente: {mnemonic}</Text>
+            <Text style={styles.mnemonic}>
+              Frase-semente: {mnemonicOpenned}
+            </Text>
             <TouchableOpacity
               style={styles.copyButton}
-              onPress={() => copyToClipboard(mnemonic)}
+              onPress={() => copyToClipboard(mnemonicOpenned)}
             >
               <Text style={styles.copyButtonText}>Copiar</Text>
             </TouchableOpacity>
@@ -106,29 +166,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    gap: 20,
   },
-
   copyButton: {
     padding: 6,
     backgroundColor: "#007bff",
     borderRadius: 4,
   },
-
   copyButtonText: {
     color: "#fff",
     fontSize: 12,
   },
-
   row: {
     flexDirection: "row",
     alignItems: "center",
     marginTop: 5,
   },
-
   header: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 20,
   },
   loadingContainer: {
     marginTop: 20,
@@ -141,7 +197,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   walletList: {
-    maginTop: 20,
+    marginTop: 20,
     width: "100%",
   },
   walletCard: {
@@ -169,5 +225,35 @@ const styles = StyleSheet.create({
     marginTop: 5,
     color: "#777",
     flex: 1,
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  input: {
+    height: 80,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    textAlignVertical: "top",
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
